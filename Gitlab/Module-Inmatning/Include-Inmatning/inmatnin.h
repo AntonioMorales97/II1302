c0576e4f598181f2d72a679e814b4c06f16892e3
August Ronne <augustronne@yahoo.se>
2019-03-26 12:58:13 +0100

    [inmatning] initial commit

---
+++ Gitlab/Module-Inmatning/Include-Inmatning/inmatnin.h
@@ -0,0 +1,9 @@
+/* inmatnin.h */
+#include "ranta.h"
+#include <stdio.h>
+#include <conio.h> /* ej ANSI, clrscr() används */
+#if !defined(inmatnin_h)
+#define inmatnin_h
+void Inmatning( double* kapitalPek, int* antalArPek );
+#endif
+
