69cf2386ffea7d316bc3f557ff7cd7f36f62d935
Antonio <antonio_97@live.se>
2019-04-25 11:29:53 +0200

    added route to hardware

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use((req, res, next) => {
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+app.use('/hardware', require('./routes/hardware'));
 
 const PORT = process.env.PORT || 5000;
 
