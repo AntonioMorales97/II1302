b4051220652acd850ea8b5bf99029477f7423bd9
Antonio <antonio_97@live.se>
2019-05-02 10:20:28 +0200

    test ibm app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
@@ -10,14 +10,14 @@ const app = express();
 // Passport config
 require('./config/passport')(passport);
 
-/*
+
 // DB Config
-const db = require('./config/keys').MongoURI;
+const db = require('./config/keys').MONGO_URI;
 // Connect to Mongo
 mongoose.connect(db, { useNewUrlParser: true, useFindAndModify: false })
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
-*/
+
 
 // EJS
 app.use(expressLayouts);
@@ -62,9 +62,9 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: err.message });
 });
 
-/*
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
-*/
+
 module.exports = app;
