1f423bebeb813816c60eac0f33884a5e5a1a2b97
Antonio Morales <35844399+AntonioMorales97@users.noreply.github.com>
2019-05-28 22:46:13 +0200

    removed mongodb link as it no longer exists

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_URI: 'mongodb+srv://ii1302:ii1302@ii1302cluster-olj2m.azure.mongodb.net/ii1302?retryWrites=true',
-    SECRET_KEY: 'anders123' //Not so secret
+    MONGO_URI: '',
+    SECRET_KEY: '123' //Not so secret
 }
 
